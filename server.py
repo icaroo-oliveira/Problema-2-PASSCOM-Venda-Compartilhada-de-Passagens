3d69702ac7c0421153c5a744fefc157a4dd0f3b0
icaroo-oliveira <icarooo10.j@gmail.com>
2024-10-10 21:17:44 -0300

    feat: criando API, versao inicial

--- server.py
+++ server.py
@@ -1,231 +1,113 @@
-import threading
+from flask import Flask, request, jsonify
 import json
+import threading
 import queue
-from connection import config_server, receber_mensagem, encerrar_conexao, testa_conexao, get_ip_address
 from utils_server import *
 
-#IP = get_ip_address('enp3s0f0')
-IP = 'localhost'
+app = Flask(__name__)
 
-PORTA = 65433
-
-# Condition para controlar acesso a fila de threads (adicionar e retirar threads na fila)
-condition = threading.Condition()
-
-# Lock separado para a região crítica (acesso a arquivos e afins)
+# Condição e Lock para controle de threads e acesso à região crítica
+#condition = threading.Condition()
 lock = threading.Lock()
 
-# Fila para garantir a ordem de acesso das threads a região crítica (FIFO)
-waiting_queue = queue.Queue()
+# Fila de threads (FIFO)
+#waiting_queue = queue.Queue()
 
-# Thread que opera cliente individualmente
-# Parâmetros ->     conexao_socket: socket da comunicação com cliente
-#                   client_address: tupla contendo IP e Porta do cliente
-# Retorno ->        Sem retorno
-def handle_client(conexao_socket, client_address):
-    try:
-        print(f"Conectado a {client_address}")
 
-        # Espera resposta do socket conexão
-        data = receber_mensagem(conexao_socket)
 
-        # Se receber algum dado, opera
-        # Se não receber nenhum dado, vai pro finally e encerra conexão
-        if data:
-            flag, origem, destino, cpf, caminho = data.decode('utf-8').split(',', 4)
 
-            # Thread atual
-            current_thread = threading.current_thread()
-            
-            # Se cliente enviou flag Caminhos, servidor retorna caminhos de origem a destino
-            if flag == "Caminhos":
-                origem = cidades[int(origem) - 1]
-                destino = cidades[int(destino) - 1]
+# se for usar trhead, coloca a logica das FIFOS para thread e codition aq dentro
+#funcao = funcao que quero executar...
+def process_threaded_task(funcao, *args):
 
-                print(f"Recebido a flag: {flag}")
-                print(f"Recebido a origem: {origem}")
-                print(f"Recebido o destino: {destino}")
+    #     current_thread = threading.current_thread()
+    #     adicionar_thread_fila(condition, current_thread, waiting_queue)
+    #     with lock:
+    #     result = task_function(*args)
+    #     remover_thread_fila(condition, current_thread, waiting_queue)
 
-                # Adiciona thread a fila e verifica se ela é a primeira da fila
-                adicionar_thread_fila(condition, current_thread, waiting_queue)
-                
-                # Região crítica - Outra thread não pode mexer no arquivo enquanto tiver carregando informação do arquivo e
-                # enviando ao cliente
-                with lock:
-                    print(f"Thread {current_thread.name} acessando a região crítica.")
+  
+    result = funcao(*args)
+    
+    return result
 
-                    G = carregar_grafo()
-                    caminhos = encontrar_caminhos(G, origem, destino)
 
-                    # Nessa parte aqui, antes de enviar ao cliente, o servidor vai adicionar numa lista, os caminhos
-                    # encontrados pelo Server "A", "B" e "C", e vai enviar essa lista
-                    # Exemplo: [
-                    #               [ "A", ["curitiba", "bh", "rj", "sao paulo"], ["curitiba", "cuiabá", "sao paulo"] ],
-                    #               [ "B", ["curitiba", "bh", "rj", "sao paulo"], ["curitiba", "cuiabá", "sao paulo"] ],
-                    #               [ "C", ["curitiba", "bh", "rj", "sao paulo"], ["curitiba", "cuiabá", "sao paulo"] ]
-                    #          ]
-                    # Logo, lista[0] = caminhos do server A; lista[1] = caminhos do server B; lista[2] = caminhos do server C
-                    # Não necessariamente nessa ordem!!!!!
+#para acesso aos caminhos de {origem} até {destino}
+@app.route('/caminhos', methods=['GET'])
+def handle_caminhos():
+    #transformando em dic 
+    data = request.json
+    #fazendo mesmo que antes
+    origem = data['origem']
+    destino = data['destino']
+    origem = cidades[int(origem) - 1]
+    destino = cidades[int(destino) - 1]
 
-                    # É preciso verificar se a lista retornada pelos servidores é vazia, se for, nem adiciona na lista e nem
-                    # envia ao cliente
+    # Processo de encontrar caminhos com threading
+    def tarefa_caminhos(origem, destino):
+    
+        G = carregar_grafo()
+        
+        caminhos = encontrar_caminhos(G, origem, destino)
+        return caminhos
 
-                    # Lista de caminhos. Cada item da lista = Servidor + caminhos encontrados
-                    serializa = json.dumps(caminhos)
+    caminhos = process_threaded_task(tarefa_caminhos, origem, destino)
 
-                    mensagem = f"Caminhos_Encontrados,{serializa}"
+    print(caminhos)
+    return jsonify({"caminhos_encontrados": caminhos})
 
-                    # Verifica se cliente deu close() (encerrou conexão)
-                    # Se não encerrou, envia os caminhos encontrados
-                    teste = testa_conexao(conexao_socket, mensagem)
 
-                    verifica_teste(teste, "Caminhos enviados com sucesso")
-                    
-                    # Remove thread atual da fila e notifica as outras threads
-                    remover_thread_fila(condition, current_thread, waiting_queue)
 
-            # Se cliente enviou flag Comprar, servidor retorna sucesso de compra ou novos caminhos
-            elif flag == "Comprar":
-                print(f"Recebido a flag: {flag}")
-                print(f"Recebido o caminho: {caminho}")
-                print(f"Recebido o cpf: {cpf}")
 
-                # Uma tupla contendo lista dos servidores de cada trecho e lista de trechos (forma caminho)
-                caminho = json.loads(caminho)
+#para regiistrar a compra de uma pasagem
+@app.route('/caminhos', methods=['POST'])
+def handle_comprar():
+    data = request.json
+    caminho = data['caminho']
+    cpf = data['cpf']
+    
 
-                # aqui ele vai desempacotar a tupla recebida e retornar 3 lista. Cada lista irá conter os trechos pertencidos
-                # a cada um dos 3 servidores. Depois vai enviar as listas para cada servidor para verificar se os trechos ainda tao
-                # disponíveis e se tiver, confirma compra
-                
-                # Primeiro item da lista = origem
-                origem = caminho[1][0]
+    #isso é novo, é retornando uma lista, mas é preciso que com essa lista se construa um caminho
+    #['cuiabá', 'sao paulo', 'minas', 'salvador'] vira --> [('cuiabá', 'sao paulo'), ('sao paulo', 'minas'), ('minas', 'salvador')]
+    pares_consecutivos = []
+    for i in range(len(caminho) - 1):
+        par = (caminho[i], caminho[i + 1]) 
+        pares_consecutivos.append(par)
 
-                # Ultimo item da lista = destino
-                destino = caminho[1][len(caminho[1]) - 1]
-
-                # Adiciona thread a fila e verifica se ela é a primeira da fila
-                adicionar_thread_fila(condition, current_thread, waiting_queue)
-                
-                # Região crítica - Outra thread não pode mexer no arquivo enquanto tiver carregando informação do arquivo ou
-                # gravando informação nele, e enviando ao cliente
-                with lock:
-                    print(f"Thread {current_thread.name} acessando a região crítica.")
-
-                    # Pega informação atual do grafo
-                    G = carregar_grafo()
-
-                    # Verifica se caminho ainda ta disponível
-                    comprar = verifica_trechos_escolhidos(G, caminho)
-                    
-                    # Se retornou false, caminho não ta mais disponível
-                    # encontra e retorna novos caminhos para cliente (grafo atualizado)
-                    if comprar == False:
-                        caminhos = encontrar_caminhos(G, origem, destino)
-
-                        # Lista de tuplas atualizada. Cada tupla = Um caminho
-                        serializa = json.dumps(caminhos)
-
-                        mensagem = f"Novos_Caminhos_Encontrados,{serializa}"
-
-                        # Verifica se cliente deu close() (encerrou conexão)
-                        # Se não encerrou, envia os novos caminhos encontrados
-                        teste = testa_conexao(conexao_socket, mensagem)
-
-                        verifica_teste(teste, "Novos caminhos enviados com sucesso")
-                    
-                    # Se retornou true, caminho ta disponível
-                    else:
-                        # Registra compra
-                        registra_trechos_escolhidos(G, caminho, cpf)
-
-                        mensagem = f"Compra_Feita,"
-
-                        # Verifica se cliente deu close() (encerrou conexão)
-                        # Se não encerrou, envia informação indicando exito na compra
-                        teste = testa_conexao(conexao_socket, mensagem)
-
-                        verifica_teste(teste, "Compra registrada com sucesso")
-                    
-                    # Remove thread atual da fila e notifica as outras threads
-                    remover_thread_fila(condition, current_thread, waiting_queue)
-            
-            # Se cliente enviou flag Passagens_Compradas, servidor retorna passagens encontradas ou nenhuma passagem encontrada
-            elif flag == "Passagens_Compradas":
-                print(f"Recebido a flag: {flag}")
-                print(f"Recebido o cpf: {cpf}")
-
-                # Adiciona thread a fila e verifica se ela é a primeira da fila
-                adicionar_thread_fila(condition, current_thread, waiting_queue)
-
-                # Região crítica - Outra thread não pode mexer no arquivo enquanto tiver carregando informação do arquivo ou
-                # gravando informação nele, e enviando ao cliente
-                with lock:
-                    print(f"Thread {current_thread.name} acessando a região crítica.")
-
-                    # Pode vir vazio se não encontrou passagens no CPF
-                    compras = verifica_compras_cpf(cpf)
-
-                    # Lista de dicionários. Cada dicionário = Uma compra de determinado CPF
-                    serializa = json.dumps(compras)
-
-                    mensagem = f"Passagens_Encontradas,{serializa}"
-
-                    # Verifica se cliente deu close() (encerrou conexão)
-                    # Se não encerrou, envia informação indicando exito na compra
-                    teste = testa_conexao(conexao_socket, mensagem)
-
-                    verifica_teste(teste, "Passagens encontradas enviadas com sucesso")
-
-                    # Remove thread atual da fila e notifica as outras threads
-                    remover_thread_fila(condition, current_thread, waiting_queue)
-
-            # Se cliente enviou flag inválida (não corresponde a nenhuma operação), 
-            # servidor retorna informação referente
+    #mesma coisa de antes, so não procura mais por outros trechos...
+    def tarefa_comprar(pares_consecutivos, cpf):#tava caminho antes
+        G = carregar_grafo()
+        with lock:
+            if verifica_trechos_escolhidos(G, pares_consecutivos):
+                print("alalalalalal")
+                registra_trechos_escolhidos(G, pares_consecutivos, cpf)
+                return "Compra realizada com sucesso"
             else:
-                print(f"Recebido a flag: {flag}")
-                print("Operação não identificada.")
+                return "Caminho indisponível"
 
-                mensagem = f"Flag_Invalida,"
+    resultado = process_threaded_task(tarefa_comprar, caminho, cpf)
 
-                # Verifica se cliente deu close() (encerrou conexão)
-                # Se não encerrou, envia informação indicando flag inválida recebida
-                teste = testa_conexao(conexao_socket, mensagem)
+    return jsonify({"resultado": resultado})
 
-                verifica_teste(teste, "Operação não identificada enviada com sucesso")
 
-    finally:
-        encerrar_conexao(conexao_socket)
-        print("\nConexão encerrada. Aguardando nova conexão...\n")
+#método pra visualizar as passagens por cpf...(arrumar)
+@app.route('/passagens', methods=['GET'])
+def handle_passagens_compradas():
+    data = request.json
+    cpf = data['cpf']
+    print(cpf)
+    # Processo de verificar passagens com threading
+    def tarefa_passagens(cpf):
+        compras = verifica_compras_cpf(cpf)
+        return compras
 
-def start_server():
-    # Se não conseguir criar e configurar servidor, encerra programa
-    server_socket = config_server(IP, PORTA)
-    if server_socket is None:
-        print("Erro ao iniciar o servidor. Encerrando aplicação.")
-        return
-    
-    # Se não existir o arquivo, cria um novo
-    cria_arquivo_grafo()
-    
-    # Retorna IP e Porta associada ao socket do servidor
-    print(f"\nServidor -> IP: {server_socket.getsockname()[0]}  |  Porta: {server_socket.getsockname()[1]}\n")
-    print("Aguardando conexão...")
+    passagens = process_threaded_task(tarefa_passagens, cpf)
+    return jsonify({"passagens_encontradas": passagens})
 
-    # Fica procurando solicitações de conexão constantemente
-    while True:
-        try:
-            # Bloqueia a execução do programa até alguém se conectar
-            # Retorna socket da conexão com cliente e informações sobre o cliente (IP e porta)
-            conexao_socket, client_address = server_socket.accept()
-
-            # Define um timeout de 10 segundos e cria uma thread para lidar com a nova conexão
-            conexao_socket.settimeout(10)
-            client_thread = threading.Thread(target=handle_client, args=(conexao_socket, client_address))
-            client_thread.start()
-
-        # Se não conseguir se conectar com um cliente, volta a procurar conexões
-        except (OSError, Exception) as e:
-            print(f"Erro ao aceitar conexão: {e}.")
 
 if __name__ == "__main__":
-    start_server()
+    # O Flask usa `run` para iniciar o servidor HTTP
+    cria_arquivo_grafo()
+    app.run(host='0.0.0.0', port=65433)
+
+
